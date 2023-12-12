182411e1bd0cfb0476ccdc47947440a4b8eb87ff
reddyshanthosh <61100281+reddyshanthosh@users.noreply.github.com>
2023-12-12 16:08:18 -0600

    working edit and create opertions

--- resources/js/pages/foods/Index.vue
+++ resources/js/pages/foods/Index.vue
@@ -1,88 +1,88 @@
 <template>
 <div>
-<v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
-  <template v-slot:top>
-    <v-toolbar flat>
-      <v-toolbar-title>My CRUD</v-toolbar-title>
-      <v-divider class="mx-4" inset vertical></v-divider>
-      <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" max-width="500px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-            New Item
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">{{ formTitle }}</span>
-          </v-card-title>
+  <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+              New Item
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
 
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field v-model="editedItem.iron" label="Iron (g)"></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="close">
-              Cancel
-            </v-btn>
-            <v-btn color="blue darken-1" text @click="save">
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-      <v-dialog v-model="dialogDelete" max-width="500px">
-        <v-card>
-          <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-            <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
-  </template>
-  <template v-slot:item.actions="{ item }">
-    <v-icon small class="mr-2" @click="editItem(item)">
-      mdi-pencil
-    </v-icon>
-    <v-icon small @click="deleteItem(item)">
-      mdi-delete
-    </v-icon>
-  </template>
-  <template v-slot:no-data>
-    <v-btn color="primary" @click="initialize">
-      Reset
-    </v-btn>
-  </template>
-</v-data-table>
-<v-snackbar v-model="snackbar" :timeout="9000" :color="snackcolor">
-  {{ snacktext }}
-  <v-btn color="white" text @click="snackbar = false">Close</v-btn>
-</v-snackbar>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.iron" label="Iron (g)"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">
+                Cancel
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="save">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon small @click="deleteItem(item)">
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+  <v-snackbar v-model="snackbar" :timeout="9000" :color="snackcolor">
+    {{ snacktext }}
+    <v-btn color="white" text @click="snackbar = false">Close</v-btn>
+  </v-snackbar>
 </div>
 </template>
 
@@ -134,7 +134,7 @@ export default {
       fat: 0,
       carbs: 0,
       protein: 0,
-      iron:0
+      iron: 0
 
     },
     defaultItem: {
@@ -143,7 +143,7 @@ export default {
       fat: 0,
       carbs: 0,
       protein: 0,
-      iron:0
+      iron: 0
     },
   }),
 
@@ -207,7 +207,7 @@ export default {
       this.closeDelete()
     },
 
-    close() { 
+    close() {
       this.dialog = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
@@ -226,9 +226,7 @@ export default {
     async save() {
       if (this.editedIndex > -1) {
         Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        console.log(this.editedItem)
-        await axios.post('api/foods/' + this.editedItem).then(response => {
+        await axios.put('api/foods/' + this.editedItem.id, this.editedItem).then(response => {
           if (response.status === 200) {
             this.desserts.splice(this.editedIndex, 1)
             this.snackbar = true;
@@ -241,7 +239,21 @@ export default {
           }
         })
         this.desserts.push(this.editedItem)
-        console.log(this.desserts)
+      } else {
+        console.log(this.editedItem)
+        await axios.post('api/foods/', this.editedItem).then(response => {
+          if (response.status === 200) {
+            this.desserts.splice(this.editedIndex, 1)
+            this.snackbar = true;
+            this.snackcolor = 'success';
+            this.snacktext = 'The Food was added successfully.';
+          } else {
+            this.snackbar = true;
+            this.snackcolor = 'error';
+            this.snacktext = 'An error occurred.  The Food was not added.';
+          }
+        })
+        this.desserts.push(this.editedItem)
       }
       this.close()
     },
